adb112b39376970058ee51d98bcb6115907a1c5d
Shameness <burakkorkmaz03@gmail.com>
2020-11-18 11:25:49 +0300

    Added Hash function

--- scripts.js
+++ scripts.js
@@ -192,6 +192,17 @@ function stringifySolutionSteps(solutions){
   return result
 }
 
+//Java Hash implementation
+const getHash = (string) => {
+  let hash = 0;
+  for (let i = 0; i < this.length; i++) {
+      let char = this.charCodeAt(i);
+      hash = ((hash<<5)-hash)+char;
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
 function main(input){
   var successfulSolutions = []
   var solutionQueue = []
